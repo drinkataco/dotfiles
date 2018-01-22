a364743eb8959ffec591f23fb6190d57680772b1
J J Walwyn <me@joshwalwyn.com>
2018-01-22 23:30:54 +0000

    Adjusted setup script to allow for silent mode and to capture argument

--- setup.sh
+++ setup.sh
@@ -4,19 +4,22 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Move dotfiles
-
 DEFAULT_DIR='~'
 
+# if not silent mode, ask for directory
 if [ "$1" != "-s" ]; then
     printf "Dotfiles Directory [$DEFAULT_DIR] : "
     read MOVE_DIR
+
+# otherwise, it is silent, try and get argument
+elif [ ! -z "$2" ]; then
+    MOVE_DIR=$2
 fi
 
+# Set default dir if none set
 if [ -z "$MOVE_DIR" ]; then
     MOVE_DIR=$DEFAULT_DIR
 fi
 
-SCRIPT_NAME=`basename "$0"`
-
 # Move all dot files
-cp ./.* $MOVE_DIR
+echo "cp ./.* $MOVE_DIR"
