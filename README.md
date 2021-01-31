bec80bb8c511ea0c2e3246037a630a02d8ea4b15
Josh Walwyn <me@joshwalwyn.com>
2021-01-31 11:23:27 +0000

    Added simple symlinking

--- README.md
+++ README.md
@@ -1,12 +1,3 @@
 # dotfiles
 This repository includes a list of common dotfiles used on my workspaces.
 
-In relation to the dotfiles, a bundling script is included that supports:
-- Building dotfiles (using envsubst)
-- Bootstrapping systems for usage
-
-Bootstrapping is done using POSIX-compliant scripts – in case bash 5> isn't installed (as is the default with macos).
-
-## TODO:
-1. entrypoint must be POSIX to bootstrap first if requried
-2. add readme
---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+BASE_DIR=$(cd -- "$(dirname -- "$0")" && pwd)
+SCRIPTS_DIR="${BASE_DIR}/scripts"
+DOTFILES_DIR="${BASE_DIR}/dotfiles"
+readonly BASE_DIR
+readonly SCRIPTS_DIR
+
+#########################################
+# Symlinks dotfiles 
+# Globals:
+#  DOTFILES_DIR
+# Arguments:
+#   None
+# Returns:
+#   None
+#######################################
+link_dotfiles() {
+  find "${DOTFILES_DIR}" | while read -r file; do
+    echo ln -s "$file" "$HOME"
+  done
+}
+
+#########################################
+# Main method to run script and find the bash script it must run
+# Globals:
+#   BASE_DIR
+#   SCRIPTS_DIR
+# Arguments:
+#   ...args - consult readme
+#######################################
+main() {
+  # Get system type
+  readonly machine=$(uname -s)
+
+  case "${machine}" in
+    Darwin*)
+      type=macos
+    ;;
+    Linux*)
+      type=linux
+      # TODO: check distro
+    ;;
+    *)
+      echo "Unknown System: ${machine}"
+      exit 1
+    ;;
+  esac
+
+  # Check parameters for operations
+  link_dotfiles
+}
+
+main "${@}"
