cb18d1b237244baa4903ca34a0233aa1f37e322f
Josh Walwyn <me@joshwalwyn.com>
2021-01-31 12:49:18 +0000

    Introduced boostrapping scripts

--- install.sh
+++ install.sh
@@ -1,8 +1,31 @@
 #!/bin/sh
 BASE_DIR=$(cd -- "$(dirname -- "$0")" && pwd)
 DOTFILES_DIR="${BASE_DIR}/dotfiles"
+SCRIPTS_DIR="${BASE_DIR}/scripts"
 readonly BASE_DIR
 readonly DOTFILES_DIR
+readonly SCRIPTS_DIR
+
+#########################################
+# Helper to determine user input for yes/no questions.
+#   Anything beginning with the letter n is a no, 
+#   everything else is yes
+# Arguments:
+#   1 - The user set value to determine whether yes or no
+#   2 - The default value if the user didn't set one
+# Returns:
+#   0 for yes, 1 for no. Inverted values so that the
+#     function can be used inline if statments
+#######################################
+yes_no() {
+  value=$1 
+  [ -z $value ] && value=$2 # Set as default if unset
+
+  case "$value" in
+    n*|N*) return 1 ;;
+    *) return 0 ;;
+  esac
+}
 
 #########################################
 # Symlinks dotfiles 
@@ -15,7 +38,7 @@ readonly DOTFILES_DIR
 #######################################
 link_dotfiles() {
   find "${DOTFILES_DIR}" -maxdepth 1 -mindepth 1 | while read -r file; do
-    echo ln -s "$file" "$HOME"
+    echo ln -v -s "$file" "$HOME"
   done
 }
 
@@ -25,28 +48,34 @@ link_dotfiles() {
 #   BASE_DIR
 #   SCRIPTS_DIR
 # Arguments:
-#   ...args - consult readme
+#   NONE
 #######################################
 main() {
-  # Get system type
-  readonly machine=$(uname -s)
+  # Check parameters for operations
+  read -p 'Do you want to symlink all dotfiles? [Y/n] ' symlink_dotfiles
+  if yes_no "$symlink_dotfiles" 'Y'; then
+    link_dotfiles
+  fi
 
-  case "${machine}" in
+  read -p 'Do you want to boostrap your machine? [y/N] ' bootstrap_machine
+
+  if ! yes_no "$bootstrap_machine" 'N'; then
+    exit 0
+  fi
+
+  case $(uname -s) in
     Darwin*)
-      type=macos
+      "${SCRIPTS_DIR}/macos.sh"
     ;;
     Linux*)
-      type=linux
-      # TODO: check distro
+      "${SCRIPTS_DIR}/linux.sh"
     ;;
     *)
       echo "Unknown System: ${machine}"
+      echo 'Your system might not be properly supported'
       exit 1
     ;;
   esac
-
-  # Check parameters for operations
-  link_dotfiles
 }
 
 main "${@}"
---
+++ scripts/macos.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+echo
+echo 'Boostrapping macOs'
+echo
+echo
+echo "                    'c.       "
+echo "                 ,xNMM.       "
+echo "               .OMMMMo        "
+echo "               OMMM0,         "
+echo "     .;loddo:' loolloddol;.   "
+echo "   cKMMMMMMMMMMNWMMMMMMMMMM0: "
+echo " .KMMMMMMMMMMMMMMMMMMMMMMMWd. "
+echo " XMMMMMMMMMMMMMMMMMMMMMMMX.   "
+echo ";MMMMMMMMMMMMMMMMMMMMMMMM:    "
+echo ":MMMMMMMMMMMMMMMMMMMMMMMM:    "
+echo ".MMMMMMMMMMMMMMMMMMMMMMMMX.   "
+echo " kMMMMMMMMMMMMMMMMMMMMMMMMWd. "
+echo " .XMMMMMMMMMMMMMMMMMMMMMMMMMMk"
+echo "  .XMMMMMMMMMMMMMMMMMMMMMMMMK."
+echo "    kMMMMMMMMMMMMMMMMMMMMMMd  "
+echo "     ;KMMMMMMMWXXWMMMMMMMk.   "
+echo "       .cooc,.    .,coo:.     "
+echo
+echo
+echo 'TODO'
+echo '1. enable Xcode'
+echo '2. Install homebrew'
+echo '3. brew bundle'
+echo '4. initialise finder'
