5fc26cc86325e3ae631426454e151b271ec0ad50
J J Walwyn <me@joshwalwyn.com>
2023-02-06 16:52:35 +0000

    FZF <C-T> to use default bat settings

--- .zsh/completions.zsh
+++ .zsh/completions.zsh
@@ -10,7 +10,7 @@ autoload -U compinit && compinit # zsh-completions reload
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git --no-ignore'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS="
-  --preview 'bat -n --color=always {}'
+  --preview 'bat {}'
   --bind 'ctrl-/:change-preview-window(down|hidden|)'"
 export FZF_CTRL_R_OPTS="
   --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
